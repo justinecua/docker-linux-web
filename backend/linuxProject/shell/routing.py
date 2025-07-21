922154c9b7dc4ea1036b087c772dd467c3ba01e7
justinecua <jcua.dev@gmail.com>
2025-07-22 01:24:26 +0800

    modified routing.py

--- backend/linuxProject/shell/routing.py
+++ backend/linuxProject/shell/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/terminal/$', consumers.TerminalConsumer.as_asgi()),
+    re_path(r'^ws/terminal/?$', consumers.TerminalConsumer.as_asgi()),
 ]
